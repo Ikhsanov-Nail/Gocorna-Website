137d85664dcbd635aa4fda6ea4cdefff01e000d9
Ikhsanov-Nail <ikhsanov.nail@gmail.com>
2022-09-05 22:05:52 +0500

    Made burger menu, adapted header

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-	const menu = document.querySelector('.menu'),
-	menuItem = document.querySelectorAll('.menu_item'),
-	hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.menu__list'),
+	menuItem = document.querySelectorAll('.menu__item'),
+	hamburger = document.querySelector('.menu__hamburger');
 
 	hamburger.addEventListener('click', () => {
-			hamburger.classList.toggle('hamburger_active');
-			menu.classList.toggle('menu_active');
+			hamburger.classList.toggle('menu__hamburger-active');
+			menu.classList.toggle('menu__list-active');
 	});
 
 	menuItem.forEach(item => {
 			item.addEventListener('click', () => {
-					hamburger.classList.toggle('hamburger_active');
-					menu.classList.toggle('menu_active');
+					hamburger.classList.toggle('menu__hamburger-active');
+					menu.classList.toggle('menu__list-active');
 			})
 	})
 })
